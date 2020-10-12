10d8e8ad7762aadef74813540ebe85458eb17812
cnoriega20 <c79.noriega@gmail.com>
2020-10-12 15:05:35 -0500

    Initial commit:Project structure from Spring Initialzr

---
+++ src/main/java/com/msvcs/cnlimitsmicroservice/CnLimitsMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.msvcs.cnlimitsmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CnLimitsMicroserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CnLimitsMicroserviceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/msvcs/cnlimitsmicroservice/CnLimitsMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.msvcs.cnlimitsmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CnLimitsMicroserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
