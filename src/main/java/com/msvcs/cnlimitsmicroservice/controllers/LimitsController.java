a74b6d561d2e573c15bbfd959d2ac60e47d479f7
cnoriega20 <c79.noriega@gmail.com>
2020-10-12 20:56:24 -0500

    Harcoded limits corrections.

--- src/main/java/com/msvcs/cnlimitsmicroservice/controllers/LimitsController.java
+++ src/main/java/com/msvcs/cnlimitsmicroservice/controllers/LimitsController.java
@@ -1,11 +1,15 @@
 package com.msvcs.cnlimitsmicroservice.controllers;
 
 import com.msvcs.cnlimitsmicroservice.beans.LimitConfiguration;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 public class LimitsController {
+
+
 
     @GetMapping("/limits")
     public LimitConfiguration getLimitFromConfig(){
