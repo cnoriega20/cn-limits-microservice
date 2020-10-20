0c378b8a8a57d5dcbf6bd23183026c343e697d2d
cnoriega20 <c79.noriega@gmail.com>
2020-10-20 02:50:39 -0500

    Register cn-limits-microservice to Eureka Naming Server.

--- src/main/java/com/msvcs/cnlimitsmicroservice/CnLimitsMicroserviceApplication.java
+++ src/main/java/com/msvcs/cnlimitsmicroservice/CnLimitsMicroserviceApplication.java
@@ -2,8 +2,10 @@ package com.msvcs.cnlimitsmicroservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class CnLimitsMicroserviceApplication {
 
     public static void main(String[] args) {
